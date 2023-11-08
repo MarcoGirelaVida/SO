b509c8f33b966b5ad6a76f5020f5b184aae76f2b
Marco_Girela <marcogirelavida@gmail.com>
2023-11-08 10:50:36 +0100

    Add error handling and directory opening functionality

--- Modulo2/Sesion2/ej2.2.c
+++ Modulo2/Sesion2/ej2.2.c
@@ -10,16 +10,30 @@
 
 int main(int argc, char*argv[])
 {
+    // ------ FASE LECTURA ------- //
     if (argc < 3)
     {
-        perror("\nError, número de argumentos incorrectos\n");
-        exit(-1);
+        fprintf(stderr, "\nError, número de argumentos incorrectos\n");
+        exit(EXIT_FAILURE);
     }
     
     char *endptr;
-    char dirpath[] = argv[1];
-    int mode = (int)strtol(argv[2],endptr,8);
+    const char dirpath[] = argv[1];
+    const int mode = (int)strtol(argv[2],endptr,8);
+    //strtol es para pasar de string a un sistema numerico, como atoi pero que sirve también para binario, octal...q
     // El endptr te indica cuál es el último bit transformado,
     // sirve para comprobar si se han transformado todos.
     // El 8 indica la base (octal), si quieres decimal pones 10
+    if (endptr == dirpath) {
+        fprintf(stderr, "Cadena vacía\n");
+        exit(EXIT_FAILURE);
+    }
+    if (*endptr != '\0')
+        exit(EXIT_FAILURE);
+
+
+    // ----- FASE OPERACIONES ------ //
+    
+    // 1. Abrimos el directorio y lo recorremos
+    opendir(dirpath);
 }
